2f567bfdedf7de0fa36f404d4a6664a0cefe9fc3
Pham Chu Duong <pduong244@gmail.com>
2023-02-10 16:15:32 +0700

    Setup Redux

---
+++ src/redux/reducer/userReducer.js
@@ -0,0 +1,21 @@
+import { INCREMENT, DECREMENT } from '../action/userActions';
+
+const INITIAL_STATE = {
+    count: 0,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state, count: state.count + 1,
+            };
+        case DECREMENT:
+            return {
+                ...state, count: state.count - 1,
+            };
+        default: return state;
+    }
+};
+
+export default userReducer;
