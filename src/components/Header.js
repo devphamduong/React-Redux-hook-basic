34606f508ce83afdd8feb955ed82c5dd25875046
Pham Chu Duong <pduong244@gmail.com>
2023-02-14 11:00:56 +0700

    Count Users - Chia sẻ data Redux (tương tự đếm items trong Cart)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useSelector } from 'react-redux';
 
 function Header(props) {
+    const listUsers = useSelector(state => state.user.listUsers);
+
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -11,18 +14,14 @@ function Header(props) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
+                        <NavDropdown className='mx-5' title={`${listUsers.length} Users`} id="basic-nav-dropdown">
+                            {listUsers && listUsers.length > 0 &&
+                                listUsers.map((item, index) => {
+                                    return (
+                                        <NavDropdown.Item key={`user-${index}`} href="#action/3.1">{item.email}</NavDropdown.Item>
+                                    );
+                                })
+                            }
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
